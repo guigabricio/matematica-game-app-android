a38d405cb955027e16f54fa247c62a56bcfb6690
Guilherme Gabricio <guigabricio@gmail.com>
2021-10-31 11:50:26 -0300

    first

---
+++ app/src/main/java/br/com/dev4education/appmatheducation/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.dev4education.appmatheducation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //teste90
+    }
+}
